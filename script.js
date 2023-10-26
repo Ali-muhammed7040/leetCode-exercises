40c260400f50e57dd0a8dea2105d2e2923b5acd2
Ali Muhammad <ali309muhammed@gmail.com>
2023-10-27 02:08:13 +0500

    roman to numbers

---
+++ script.js
@@ -0,0 +1,47 @@
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+
+// Example 1:
+
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+// Example 2:
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+// Example 3:
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+let numbers = {
+    I : 1
+  ,  V : 5
+  ,  X : 10
+ ,   L : 50
+ ,   C : 100
+,    D : 500
+,    M : 1000,
+}
+function romanToInt(roman){
+let result = 0;
+for (let i = 0; i < roman.length; i++) {
+  if(numbers[roman[i]]< numbers[roman[i+1]]){
+    result -= numbers[roman[i]]
+  }
+  else{
+    result += numbers[roman[i]]
+  }
+}
+return result
+}
+console.log(romanToInt("III"),'final')
+console.log(romanToInt("LVIII"),'final')
+console.log(romanToInt("MCMXCIV"),'final')
